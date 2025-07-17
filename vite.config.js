cd36889f264d88ba177e0e7203b1a4b86a804399
Ashwin-Selvaraj <ashwin240899@gmail.com>
2025-07-17 18:05:13 +0530

    Coverted CRA project to vite project

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  optimizeDeps: {
+    include: ['ethers'],
+  },
+  build: {
+    rollupOptions: {
+      // DO NOT put ethers here unless you're using it as an external CDN/global import
+      external: [], // leave empty
+    },
+    commonjsOptions: {
+      include: [/ethers/, /node_modules/],
+    },
+  },
+  resolve: {
+    dedupe: ['ethers'],
+  },
+})
