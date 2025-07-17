e2018787bb526122ed7b1ab4263f9f8f5f91c65d
Ashwin-Selvaraj <ashwin240899@gmail.com>
2025-07-17 16:52:44 +0530

    Coverted CRA project to vite project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { WalletProvider } from './components/WalletProvider'
+import { WalletConnect } from './components/WalletConnect'
+
+function App() {
+  return (
+    <>
+    <WalletProvider>
+      <div className="App">
+        <WalletConnect />
+      </div>
+    </WalletProvider>
+    </>
+    
+  )
+}
+
+export default App;
